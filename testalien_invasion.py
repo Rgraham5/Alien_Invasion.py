af5c8faeec047c1a13c1b6de7eb3fa607365c413
Rgraham5 <82477614+Rgraham5@users.noreply.github.com>
2021-04-14 20:46:06 -0400

    Add files via upload
    set up for redoing testing all under one. import function finally right

---
+++ testalien_invasion.py
@@ -0,0 +1,91 @@
+import unittest
+from alien_invasion import AlienInvasion
+
+class TestAlienInvasion(unittest.TestCase):
+
+	def setUp(self):
+		pass
+	
+	def tearDown(self):
+		pass
+	
+	def Test_check_keydown_events(self):
+		# Arrange
+		# Act
+		# Assert
+		self.assertTrue(self, event, True)
+
+	def test_check_keyup_events(self):
+		# Arrange
+		event.key = K_q
+		# Act
+		result = sys.exit()
+		# Assert
+		self.assertFalse(result, event)
+
+	def test_create_alien(self):
+		# Arrange
+		# Act
+		# Assert
+
+	def test_fire_bullet(self):
+		# Arrange
+		# Act
+		# Assert
+
+	def test_update_bullet(self):
+		# Arrange
+		# Act
+		# Assert
+
+	
+
+if __name__ == '__main__':
+    unittest.main()
+
+   #def _check_keydown_events(self, event):
+		"""Key presses"""
+		#if event.key == pygame.K_RIGHT:
+			#self.ship.moving_right = True
+		#elif event.key == pygame.K_LEFT:
+			#self.ship.moving_left = True
+		#elif event.key == pygame.K_q:
+			#sys.exit()
+		#elif event.key == pygame.K_SPACE:
+		#	self._fire_bullet()
+
+	#def _check_keyup_events(self, event):
+		"""key releases"""
+		#if event.key == pygame.K_RIGHT:
+		#	self.ship.moving_right = False
+		#elif event.key == pygame.K_LEFT:
+		#	self.ship.moving_left = False 
+
+	#def _fire_bullet(self):
+		"""create a new bullet and add it to the bullets group"""
+	#	if len(self.bullets) < self.settings.bullets_allowed:
+	#		new_bullet = Bullet(self)
+	#		self.bullets.add(new_bullet)
+
+	#def _update_bullets(self):
+		"""Update position of bullets and get rid of old bullets."""
+		#Update bullet positions.
+		# Get rid of bullets that have disappeared.
+	#	self.bullets.update()
+
+
+	#	for bullet in self.bullets.copy():
+	#		if bullet.rect.bottom <= 0:
+	#			 self.bullets.remove(bullet)
+		
+	#	self._check_bullet_alien_collisions()
+
+
+	#def _create_alien(self, alien_number, row_number):
+		"""create an alien and place it in the row."""	
+		#alien = Alien(self)
+		#alien_width, alien_height = alien.rect.size
+		#alien.x = alien_width + 2 * alien_width * alien_number
+		#alien.rect.x = alien.x
+		#alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
+		#self.aliens.add(alien)	
